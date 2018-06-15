5ee019ad2737d696dc18efd9b5b131d8d24043ec
Juan Bautista Marval Hernandez <juan.marval@globant.com>
2018-06-15 17:59:46 -0300

    init repo

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+server.listen(port, () => {
+  console.log('server starter port ' + port);
+});
