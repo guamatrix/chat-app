e50dab6da46e7202c27984891d2fe8d5ec4d16aa
Juan Marval <juan.marval@synapse.com>
2018-09-21 10:22:32 -0300

    html and jquery chat start

--- public/js/index.js
+++ public/js/index.js
@@ -13,6 +13,10 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', (message) => {
   console.log('new message', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
 
 // socket.emit('createEmail', ({
@@ -30,3 +34,20 @@ socket.on('newMessage', (message) => {
 // socket.on('newEmail', (email) => {
 //   console.log('new email', email);
 // });
+
+// socket.emit('createMessage', {
+//   from: 'Client',
+//   text: 'Hi'
+// }, function(data) {
+//   console.log('got it', data);
+// });
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function() {
+
+  })
+});
--- server/server.js
+++ server/server.js
@@ -30,10 +30,11 @@ io.on('connection', (socket) => {
   //   console.log('create email', newEmail);
   // });
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('create message ', message);
 
     io.emit('newMessage', utils.generateMessage(message.from, message.text));
+    callback('this is from the server');
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
